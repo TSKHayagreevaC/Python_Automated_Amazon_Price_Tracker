1522716b5e5ba47bef96de9876d06290b2371e10
THIRUMALA SRIKANTH HAYAGREEVA CHARYA <69465021+TSKHayagreevaC@users.noreply.github.com>
2021-04-01 17:49:14 +0530

    commit with completed code to track a commercial product using apis and modules.

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+import requests
+import lxml
+from bs4 import BeautifulSoup
+import smtplib
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+url = "https://www.amazon.in/Titan-Analog-Black-Dial-Watch-1834NL01/dp/" \
+      "B07XYD1LCQ/ref=sr_1_2?dchild=1&fst=as%3Aoff&pf_rd_i=2563504031&pf_rd_m=A1VBAL9TL5WCBF&pf_rd_" \
+      "p=ea82bcc8-5c87-43c9-b2d6-ebaa0069f86b&pf_rd_r=CMSY8EX38YEG1HHRQDX1&pf_rd_" \
+      "s=merchandised-search-5&qid=1617277974&refinements=p_89%3ATitan&rnid=1350388031&s=watch&sr=1-2"
+
+header = {
+    "User-Agent": "Mozilla/5.0 (Machintosh; Intel Mac OS X 10_15_5) ApplleWebkit/537.36 (KHTML, like Gecko) Chrome/"
+                  "84.0.4147.125 Safari/537.125 Safari/537.36",
+    "Accept-Language": "en=GB, en-US;q=0.8"
+}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+MY_EMAIL = "srikanth@gmail.com"
+MY_PASSWORD = "Vagisha$299"
+response = requests.get(url, headers=header)
+
+soup = BeautifulSoup(response.content, "lxml")
+print(soup.prettify())
+
+price = soup.find(id="priceblock_ourprice").get_text()
+price_without_currency = price.split("$")[1]
+price_as_float = float(price_without_currency)
+
+title = soup.find(id="procudtTitle").get_text().strip()
+price(title)
+
+BUY_PRICE = 200
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is now {price}"
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        result = connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addr="ramana@gmail.com",
+            msg=f"Subject:Price Alert From Amazon Is Here :- \n\n{message}\n{url}"
+        )
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
